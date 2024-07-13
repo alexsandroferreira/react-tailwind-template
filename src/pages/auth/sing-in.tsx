f0c3c14fb792fe15948de079829e31d3a0e8c7eb
alexsandroferreira <ferreira.alex@outlook.com>
2024-07-13 10:20:29 -0300

    feat: add plugin react Helmet (SEO)

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SingIn() {
-  return <div>sing-in</div>
+  return (
+    <>
+      <Helmet title="Login" />
+      <div>sing-in</div>
+    </>
+  )
 }
